8e06395a30d5457f9750d1438abb9d5ecd0d2dde
KKazlov <toner7050@gmail.com>
2021-08-08 13:09:02 +0300

    cartItemControl default

--- src/component/cart-item-control/cart-item-control.js
+++ src/component/cart-item-control/cart-item-control.js
@@ -1,41 +1,19 @@
 import React from "react";
-import { connect } from "react-redux";
-import { selectingPizza } from "../actions/actions";
+
 import "./cart-item-control.css";
 
-const CartItemControl = ({ counter, pizzasGroup, allPizzas, selectingPizza }) => {
-    const pizza = pizzasGroup[0];
-    const { id, size, dough, price } = pizza;
+const CartItemControl = ({ counter, pizzasGroup }) => {
+    const { price } = pizzasGroup[0];
     const totalPrice = Math.floor(counter * price * 100) / 100;
 
     return (
         <div className="cart-item__control">
             <div className="cart-item__quantity">
-                <button
-                    className="cart-item__btn"
-                    onClick={() => {
-                        const index = allPizzas.findIndex(
-                            (item) =>
-                                `${id}${size}${dough}` ===
-                                `${item.id}${item.size}${item.dough}`
-                        );
-
-                        const before = allPizzas.slice(0, index);
-                        const after = allPizzas.slice(index + 1);
-
-                        const newArr = [...before, ...after];
-                        console.log('minus', newArr)
-                        selectingPizza(newArr)
-                    }}>
+                <button className="cart-item__btn">
                     <div className="minus" />
                 </button>
                 <span className="cart-item__counter">{counter}</span>
-                <button className="cart-item__btn" onClick={() => {
-                    const newArr = [...allPizzas, pizza];
-                    console.log('plus', newArr)
-
-                    selectingPizza(newArr);
-                }}>
+                <button className="cart-item__btn">
                     <div>+</div>
                 </button>
             </div>
@@ -49,20 +27,4 @@ const CartItemControl = ({ counter, pizzasGroup, allPizzas, selectingPizza }) =>
     );
 };
 
-const mapStateToProps = ({selectedPizzas}) => {
-    return {
-        allPizzas: selectedPizzas
-    }
-}
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        selectingPizza: (newArr) => {
-            dispatch(selectingPizza(newArr))
-        }
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(CartItemControl);
+export default CartItemControl;
